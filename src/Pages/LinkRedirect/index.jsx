41830b524dd846ed4fb1bc1bc922770b7068ed79
Karam Guliyev <karamguliyev10@gmail.com>
2023-05-20 00:25:54 +0300

    Total Click Increment

--- src/Pages/LinkRedirect/index.jsx
+++ src/Pages/LinkRedirect/index.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { firestore } from "../../firebase";
+import { app, firestore } from "../../firebase";
 import { Box, CircularProgress, Typography } from "@mui/material";
 
 const LinkRedirect = () => {
@@ -12,7 +12,10 @@ const LinkRedirect = () => {
       const linkDoc = await firestore.collection("links").doc(shortCode).get();
       setTimeout(() => {
         if (linkDoc.exists) {
-          const { longUrl } = linkDoc.data();
+          const { longUrl, linkID, userUid } = linkDoc.data();
+          firestore.collection('users').doc(userUid).collection("links").doc(linkID).update({
+            totalClicks : app.firestore.FieldValue.increment(1)
+          })
           window.location.href = `http://${longUrl}`;
         } else {
           setLoading(false);
